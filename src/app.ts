8b0fc51029e63d5c8655fa6aace0ad213f5a870c
codeSmith-006 <ryanrehan.pc@gmail.com>
2026-05-23 23:21:48 +0600

    fix vercel and apply cors

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,31 @@
-import express, { type Application, type Request, type Response } from "express";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
 import { usersRouter } from "./modules/auth/users.route";
 import { issuesRouter } from "./modules/issues/issues.route";
-
+import cors from "cors";
 
 const app: Application = express();
-
-app.use(express.json())
-
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+app.use(express.json());
 
 // initial get method response when the server is running
 app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "Issue Manager is running..."
-    })
-})
+  res.status(200).json({
+    message: "Issue Manager is running...",
+  });
+});
 
 // use - users route for auth
-app.use("/api/auth", usersRouter)
+app.use("/api/auth", usersRouter);
 
 // use - issues route
-app.use("/api/issues", issuesRouter)
-
+app.use("/api/issues", issuesRouter);
 
 export default app;
